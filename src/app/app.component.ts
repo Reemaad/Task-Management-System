9f3b2fb2fb54b83daaafbe8cc5a6f6c2633c7db8
Manal <manaltamiimii@gmail.com>
2024-04-21 09:06:56 +0300

    refactor: resolve Reema comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'Task-Management-System';
+  title = "Task-Management-System";
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,27 @@
 import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule,TranslateModule],
+  providers: [],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
 export class HeaderComponent {
   showMenueItems: boolean = false;
 
- 
+  constructor(public translate: TranslateService) {
+  }
+
   toggleMenue() {
     this.showMenueItems = !this.showMenueItems;
   }
 
-  logout() {}
+  logout() {
+    // TODO: Implement this method later (Manal)
+  }
 }
