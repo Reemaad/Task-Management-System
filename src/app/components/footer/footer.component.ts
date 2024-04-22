041ca321e2c59eae463a6fe139a89a4947d9a61c
Leena Almatar <LeenaAlmatar12@gmail.com>
2024-04-22 22:17:18 +0300

    feat: add footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent implements OnInit {
+
+  currentYear: number = new Date().getFullYear();
+  ngOnInit(): void {
+
+  }
+}
