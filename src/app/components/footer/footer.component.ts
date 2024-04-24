075b419498949d1ce3a38a3c4d8f693aba9c3eea
Leena Almatar <LeenaAlmatar12@gmail.com>
2024-04-24 10:19:03 +0300

    refactor: updated style + naming custom footer/header

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-footer',
+  selector: 'custom-footer',
   standalone: true,
   imports: [TranslateModule],
   templateUrl: './footer.component.html',
