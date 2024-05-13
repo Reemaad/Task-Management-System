13e4bff1561b2f07d2f55a4022c6e782a35f9835
LeenaAlmatar <leenaalmatar12@gmail.com>
2024-05-13 15:17:36 +0300

    refactor: resolve comments

--- src/app/components/pop-up/pop-up.component.ts
+++ src/app/components/pop-up/pop-up.component.ts
@@ -3,23 +3,24 @@ import { TranslateModule } from "@ngx-translate/core";
 import { AppComponent } from "../../app.component";
 
 @Component({
-    selector: 'popup',
-    standalone: true,
-    templateUrl: './pop-up.component.html',
-    styleUrl: './pop-up.component.css',
-    imports: [TranslateModule, AppComponent]
+  selector: 'popup',
+  standalone: true,
+  templateUrl: './pop-up.component.html',
+  styleUrl: './pop-up.component.css',
+  imports: [TranslateModule]
 })
 export class PopUpComponent {
   @Input() isPopupVisible: boolean = false;
+  @Input() title?: string;
 
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
-  
+
   openPopup() {
     this.isPopupVisible = true;
   }
 
   closePopup() {
-    this.close.emit();
     this.isPopupVisible = false;
+    this.close.emit();
   }
 }
