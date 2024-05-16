ffda13eb8f638d94d54b4fe7394e497319c64a6f
Manal <manaltamiimii@gmail.com>
2024-05-16 10:05:25 +0300

    refactor: style select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { DropdownComponent } from "./components/dropdown/dropdown.component";
-import { DropdownItem } from "./components/models/dropdown-item";
 
 @Component({
   selector: "app-root",
@@ -14,13 +12,4 @@ import { DropdownItem } from "./components/models/dropdown-item";
 })
 export class AppComponent<T> {
   title = "Task-Management-System";
-  items: DropdownItem[] = [
-    { value: "DROPDOWNـOPTIONS.OPTION1_VALUE", label: "DROPDOWNـOPTIONS.OPTION1_LABEL" },
-    { value: "DROPDOWNـOPTIONS.OPTION2_VALUE", label: "DROPDOWNـOPTIONS.OPTION2_LABEL" },
-    { value: "DROPDOWNـOPTIONS.OPTION3_VALUE", label: "DROPDOWNـOPTIONS.OPTION3_LABEL" },
-  ];
-
-  onSelectedItemChanged(value: string) {
-    console.log("Selected value:", value);
-  }
 }
