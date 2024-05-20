02b6b8cc1d7cf5a4d11ec80bb8061469a2601754
LeenaAlmatar <leenaalmatar12@gmail.com>
2024-05-20 09:30:24 +0300

    refactor: rename path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from "./pages/login/login.component";
 
 export const routes: Routes = [
     {
-        path: 'src/app/pages/login-page',
+        path: 'login',
         component: LoginComponent
     }
 import { TaskManagementPageComponent } from "./pages/task-management-page/task-management-page.component";
