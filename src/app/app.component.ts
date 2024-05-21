bb9f81d6eabb9edc03d6273bcd04487b458664d0
LeenaAlmatar <leenaalmatar12@gmail.com>
2024-05-21 09:47:23 +0300

    refactor: resolve comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,32 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { TaskManagementPageComponent } from "./pages/task-management-page/task-management-page.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent, RouterOutlet, RouterLink, RouterLinkActive]
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent, RouterLink, RouterLinkActive]
 })
-export class AppComponent {
   title = "Task-Management-System";
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateLayout(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  updateLayout(url: string) {
+    if (url === '/login') {
+      this.showHeader = false;
+    } else {
+      this.showHeader = true;
+    }
+  }
+  
 }
