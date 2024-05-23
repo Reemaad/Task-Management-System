d1ff22a00b9a086c1df83a155b4ad5bf49a8e3a4
Manal <manaltamiimii@gmail.com>
2024-05-23 12:13:30 +0300

    refactor: convert model

--- src/app/models/tasks.ts
+++ src/app/models/tasks.ts
@@ -1,12 +1,6 @@
 import { TableData } from "./table-data";
 
-export class Tasks implements TableData {
-  id: number;
+export interface Tasks extends TableData {
   status: string;
   description: string;
-  constructor(id: number, status: string, description: string) {
-    this.id = id;
-    this.status = status;
-    this.description = description;
-  }
 }
