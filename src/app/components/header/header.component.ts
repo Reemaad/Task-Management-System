a8a147898b19e3479bf8a591ef66fb78737c8529
LeenaAlmatar <leenaalmatar12@gmail.com>
2024-05-26 14:30:38 +0300

    feat: logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,41 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
-
+import { Router } from "@angular/router";
+import { PopUpComponent } from "../pop-up/pop-up.component";
+import { ButtonComponent } from "../button/button.component";
+import { ButtonType } from "../../enums/button-type";
 @Component({
   selector: "custom-header",
   standalone: true,
-  imports: [CommonModule, TranslateModule],
   providers: [],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
+  imports: [CommonModule, TranslateModule, PopUpComponent, ButtonComponent]
 })
 export class HeaderComponent {
   showMenuItems: boolean = false;
+  ButtonType = ButtonType;
+  
+  @ViewChild(PopUpComponent) popup!: PopUpComponent;
 
   toggleMenu() {
     this.showMenuItems = !this.showMenuItems;
   }
 
+  openPopup() {
+    this.toggleMenu();
+    this.popup.open();
+  }
+
+  closePopup() {
+    this.popup.closePopup();
+  }
+
+  constructor(private router: Router) { }
+
   logout() {
-    // TODO: Implement this method later (Manal)
+    // If there is Logic when Manal implement login
+    this.router.navigate(['/login']);
   }
 }
