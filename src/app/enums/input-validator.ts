0194d63f2edfe478aaabec14e183a6fc68b65cb9
Manal <manaltamiimii@gmail.com>
2024-05-27 10:00:28 +0300

    feat: login

--- src/app/enums/input-validator.ts
+++ src/app/enums/input-validator.ts
@@ -1,4 +1,5 @@
 export enum InputValidator {
   pattern,
   required,
+  maxlength,
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,34 +1,87 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { InputType } from '../../enums/input-type';
+import { Component } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import { InputType } from "../../enums/input-type";
 import { InputComponent } from "../../components/input/input.component";
 import { RouterOutlet } from "@angular/router";
 import { TranslateModule } from "@ngx-translate/core";
 import { ButtonComponent } from "../../components/button/button.component";
-import { ButtonType } from '../../enums/button-type';
+import { ButtonType } from "../../enums/button-type";
+import { InputValidator } from "../../enums/input-validator";
+import { ErrorMessage } from "../../models/error-message";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'login',
+  selector: "login",
   standalone: true,
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
-  imports: [TranslateModule, InputComponent, RouterOutlet, ReactiveFormsModule, ButtonComponent]
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
+  imports: [
+    TranslateModule,
+    InputComponent,
+    RouterOutlet,
+    ReactiveFormsModule,
+    ButtonComponent,
+  ],
 })
 export class LoginComponent {
   loginForm!: FormGroup;
   InputType = InputType;
   ButtonType = ButtonType;
 
-  constructor() {
+  constructor(private router: Router) {
     this.loginForm = new FormGroup({
       username: new FormControl("", [
         Validators.required,
-        Validators.pattern(/^(?=.*[a-zA-Z])[a-zA-Z0-9._%+-@]{3,}$/),
+        Validators.pattern(/^\d{10}$/),
       ]),
       password: new FormControl("", [
         Validators.required,
-        Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/),
+        Validators.pattern(
+          "^(?=.*[a-zA-Z0-9])(?=.*[@$%&*!])[a-zA-Z0-9@$%&*!]{8,}$"
+        ),
+        Validators.maxLength(30),
       ]),
     });
+  }
+
+
+  errorMessages: { [type: string]: ErrorMessage[] } = {
+    username: [
+      {
+        validator: InputValidator.required,
+        message: "ERROR_MESSAGE.REQUIRED",
+      },
+      {
+        validator: InputValidator.pattern,
+        message: "ERROR_MESSAGE.PATTERN_USERNAME",
+      },
+    ],
+    password: [
+      {
+        validator: InputValidator.required,
+        message: "ERROR_MESSAGE.REQUIRED",
+      },
+      {
+        validator: InputValidator.pattern,
+        message: "ERROR_MESSAGE.PATTERN_PASSWORD",
+      },
+      {
+        validator: InputValidator.maxlength,
+        message: "ERROR_MESSAGE.LENGTH_PASSWORD",
+      },
+    ],
+  };
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigate(["tasks"]);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
   }
 }
